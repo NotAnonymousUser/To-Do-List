642d25e4a5c0c4dc8eba1867aa638e01862b8253
Muhammad Mubeen Javaid <125754246+NotAnonymousUser@users.noreply.github.com>
2024-04-01 16:44:48 +0500

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todolist = [];
+let condition = true;
+while (condition) {
+    let newTask = await inquirer.prompt([
+        {
+            name: "add",
+            type: "input",
+            message: "Enter your task",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "do you want to add more tasks",
+            default: "false",
+        },
+    ]);
+    todolist.push(newTask.add);
+    condition = newTask.addMore;
+    console.log(todolist);
+}
