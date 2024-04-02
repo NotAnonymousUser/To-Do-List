355fce2310fedce9d07e1b11fb57a4b83666e9f7
Muhammad Mubeen Javaid <125754246+NotAnonymousUser@users.noreply.github.com>
2024-04-02 11:52:39 +0500

    complete program

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 let todolist = [];
 let condition = true;
 while (condition) {
-    let newTask = await inquirer.prompt([
-        {
+    let newOperation = await inquirer.prompt({
+        name: "task",
+        type: "list",
+        choices: ["Add Task", "Delete Task", "View Task"],
+    });
+    if (newOperation.task === "Add Task") {
+        let newTask = await inquirer.prompt({
             name: "add",
             type: "input",
             message: "Enter your task",
-        },
-        {
-            name: "addMore",
-            type: "confirm",
-            message: "do you want to add more tasks",
-            default: "false",
-        },
-    ]);
-    todolist.push(newTask.add);
-    condition = newTask.addMore;
+        });
+        todolist.push(newTask.add);
+        console.log(todolist);
+    }
+    else if (newOperation.task === "Delete Task") {
+        console.log(todolist);
+        let removeTask = await inquirer.prompt({
+            name: "delete",
+            type: "list",
+            choices: ["Delete First Task", "Delete Last Task"],
+        });
+        if (removeTask.delete === "Delete First Task") {
+            let firstElement = todolist.shift();
+            console.log(chalk.red(`${firstElement} is removed`));
+            console.log(todolist);
+        }
+        else if (removeTask.delete === "Delete Last Task") {
+            let lastElement = todolist.pop();
+            console.log(chalk.red(`${lastElement} is removed`));
+            console.log(todolist);
+        }
+    }
+    else if (newOperation.task === "View Task") {
+        console.log(todolist);
+    }
+    let nextOperation = await inquirer.prompt({
+        name: "moreOperation",
+        type: "confirm",
+        message: "do you want to perform more operations",
+        default: "false",
+    });
+    condition = nextOperation.moreOperation;
     console.log(todolist);
 }
